d181360a0a109c1cdd2e307d6ed72a8b797916a8
Mario Dariel <mdariel663@gmail.com>
2024-08-13 15:28:10 -0400

    Actualizacion de eliminacion de codigo inncesario

--- src/controllers/TokenManager.js
+++
@@ -1,6 +0,0 @@
-export const verificarToken = (navigate) => {
-    const token = localStorage.getItem('authToken');
-    if (!token) {
-        navigate('/');
-    }
-};
--- src/models/AuthenticationModel.js
+++
@@ -1,9 +0,0 @@
-// src/models/AuthenticationModel.js
-import axiosInstance from './AxiosModel'
-
-export const loginUser = async (username, password) => {
-    try {
-        const { data } = await axiosInstance.post('/users/login', { username, password });
-        return data;
-    } catch (error) {   throw error;  }
-};
